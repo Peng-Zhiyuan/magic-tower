51d687c2a64aec6e126a8d99bfa25e4a69a675dd
按任意键自爆 <460424301@qq.com>
2018-08-05 21:06:04 +0800

    change tile

--- assets/Script/GM/GameMaster.ts
+++ assets/Script/GM/GameMaster.ts
@@ -171,6 +171,7 @@ export default class GameMaster
     static async loadMap(map: number, isDown: boolean)
     {
         let path = "map/map" + map
+        //path = "map/test"
         await MapManager.loadAsync(path, isDown)
     }
     
